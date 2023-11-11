261abe7b30c92c47f170aca2e9239f633605d115
Павел Мещеряков <pavellol20@gmail.com>
2023-11-12 01:26:49 +0500

    Task7

---
+++ Tyuiu.MeshcheryakovPV.Sprint4.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MeshcheryakovPV.Sprint4.Task7.V15.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint4.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            // Task7
+            int rows = 4;
+            int colums = 2;
+            int[,] mtrx = new int[rows, colums];
+            string str = "10293847";
+            int res = ds.Calculate(rows, colums, str);
+            int wait = 4;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MeshcheryakovPV.Sprint4.Task7.V15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MeshcheryakovPV.Sprint4.Task7.V15.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint4.Task7.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Мещеряков П. В. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила: Мещеряков Павел Викторович | ИИПб-23-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 10293847. Преобразуйте                *");
+            Console.WriteLine("* ее в матрицу 4 на 2 и подсчитайте количество нечетных чисел в матрице.  *");
+            int rows = 4;
+            int colums = 2;
+            int[,] mtrx = new int[rows, colums];
+            string str = "10293847";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int index = 0;
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(rows, colums, str);
+            Console.WriteLine("Количество нечетных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
